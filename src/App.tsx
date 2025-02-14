2b2a7616d288595f403edfaa16b8bc3b7a7ca367
ricky-kiva <ricky.kiva@gmail.com>
2025-02-14 14:22:59 +0700

    feat(src): make `App` modular

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,42 @@ interface HeaderProps {
   name: string
 }
 
-interface ContentData {
-  name: string,
-  exerciseCount: number
+interface PartProps {
+  course: CoursePart;
 }
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartDescribed extends CoursePartBase {
+  description: string;
+} 
+
+interface CoursePartBasic extends CoursePartDescribed {
+  kind: 'basic';
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: 'group';
+}
+
+interface CoursePartBackground extends CoursePartDescribed {
+  backgroundMaterial: string;
+  kind: 'background';
+}
+
+interface CoursePartSpecial extends CoursePartDescribed {
+  requirements: string[];
+  kind: 'special';
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
 interface ContentProps {
-  contents: ContentData[]
+  contents: CoursePart[]
 }
 
 interface TotalProps {
@@ -19,14 +48,37 @@ const Header = (props: HeaderProps) => (
   <h1>{props.name}</h1>
 )
 
+const Part = (props: PartProps) => {
+  switch(props.course.kind) {
+    case 'basic':
+      return <p>
+        <b>{props.course.name} {props.course.exerciseCount}</b>
+        <br/><i>{props.course.description}</i>
+      </p>;
+    case 'group':
+      return <p>
+        <b>{props.course.name} {props.course.exerciseCount}</b>
+        <br/>project exercises {props.course.groupProjectCount}
+      </p>;
+    case 'background':
+      return <p>
+        <b>{props.course.name} {props.course.exerciseCount}</b>
+        <br/><i>{props.course.description}</i>
+        <br/>submit to {props.course.backgroundMaterial}
+      </p>;
+    case 'special':
+      return <p>
+        <b>{props.course.name} {props.course.exerciseCount}</b>
+        <br/><i>{props.course.description}</i>
+        <br/>required skils: {props.course.requirements.join(', ')}
+      </p>;
+    default:
+      return assertNever(props.course);
+  }
+}
+
 const Content = (props: ContentProps) => (
-  <>
-    {
-      props.contents.map((c, i) => (
-        <p key={i}>{c.name} {c.exerciseCount}</p>
-      ))
-    }
-  </>
+  <>{props.contents.map((c) => <Part key={c.name} course={c} />)}</>
 )
 
 const Total = (props: TotalProps) => (
@@ -35,18 +87,44 @@ const Total = (props: TotalProps) => (
 
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
-      name: 'Fundamentals',
-      exerciseCount: 10
+      name: "Fundamentals",
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
-      name: 'Using props to pass data',
-      exerciseCount: 7
+      name: "Using props to pass data",
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
     },
     {
-      name: 'Deeper type usage',
-      exerciseCount: 14
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
@@ -60,6 +138,10 @@ const App = () => {
       <Total total={totalExercises} />
     </div>
   )
+}
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
 }
 
 export default App;
